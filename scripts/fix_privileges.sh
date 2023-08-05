f79d5fd3e692cc9918cf8e427b81305c8b75af3c
madhav411 <madhavguvvuala@gmail.com>
2023-08-06 00:49:29 +0530

    Added a simple java project

---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod u+x /home/ec2-user/server/*.jar
+chmod u+x /home/ec2-user/server/server_start.sh
+chmod u+x /home/ec2-user/server/server_stop.sh
---
+++ src/main/java/com/pravalika/awsdemo/TestController.java
@@ -0,0 +1,13 @@
+package com.pravalika.awsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+@GetMapping("/app")
+public String app() {
+  return "Hello World from Master branch";
+}
+}
